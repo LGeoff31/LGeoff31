f9afe9dc32785042fc3566561fe55b8ca268e2d2
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-14 23:54:48 -0400

    Runtime: 99 ms (56.91%), Memory: 17.8 MB (73.78%) - Gitcode

--- 229-majority-element-ii/229. Majority Element II.py
+++ 229-majority-element-ii/229. Majority Element II.py
@@ -1,12 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        dic = Counter(nums)
+        count = defaultdict(int) #dict whre every number set to 0
 
-        a = len(nums)//3
+        for num in nums:
+            count[num] += 1
+
+            if len(count) <= 2: #valid
+                continue
+            else:
+                newCounter = defaultdict(int)
+                for key in count:
+                    if count[key] > 1:
+                        newCounter[key] = count[key] - 1
+                    # count[key] -= 1
+                    # if count[key]!=0:
+                    #     newCounter[key] = count[key]
+                count = newCounter 
+        n = len(nums) // 3
         res = []
-        for key in dic:
-            if dic[key] > a:
+
+        for key in count:
+            if nums.count(key) > n:
                 res.append(key)
-        return res
-                
         
+
+        return res
+
