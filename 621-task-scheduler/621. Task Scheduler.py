bab7f3d48f9a7ce73a8cdcfd2d10ec6ac0a35186
Geoffrey Lee <74619195+LGeoff31@users.noreply.github.com>
2024-03-18 23:34:35 -0400

    Runtime: 395 ms (73.21%), Memory: 17.4 MB (9.87%) - Gitcode

--- 621-task-scheduler/621. Task Scheduler.py
+++ 621-task-scheduler/621. Task Scheduler.py
@@ -4,9 +4,7 @@ class Solution:
         dic = Counter(tasks)
         max_heap = [-freq for freq in dic.values()]
         heapify(max_heap)
-
         time = 0
-
         while max_heap:
             temp = []
             idle = n + 1
@@ -21,24 +19,4 @@ class Solution:
             if not max_heap:
                 break
             time += idle if idle > 0 else 0
-        return time
-
-        arr = []
-        for key in dic:
-            arr.append(dic[key])
-        arr.sort(reverse=True)
-        time = 0
-        while sum(arr) != 0:
-            idle = n
-            for i in range(len(arr)):
-                if arr[i] != 0:
-                    arr[i] -= 1
-                    time += 1
-                    idle -= 1 if i != 0 else 0
-                    if idle == 0:
-                        break
-            if sum(arr) == 0:
-                break
-            if idle > 0:
-                time += idle
         return time
